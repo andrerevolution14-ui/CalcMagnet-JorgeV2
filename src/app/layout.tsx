beb0f5da471b552e58735ef7b67374d607e9b421
Andre Queiros <andrerevolution14@gmail.com>
2026-01-29 12:33:33 +0100

    Analytics: Update Microsoft Clarity tracking ID to v906xat28d

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,7 +26,7 @@ export default function RootLayout({
                   c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
                   t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
                   y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-              })(window, document, "clarity", "script", "h8i8n8v8j8");
+              })(window, document, "clarity", "script", "v906xat28d");
             `,
           }}
         />
